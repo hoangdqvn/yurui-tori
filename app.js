b87afea90c26c10701f325c34b3e9e82e5d1f376
hoangdq <hoangdq.vnist@gmail..com>
2020-06-11 07:39:44 +0700

    format code

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
@@ -20,9 +19,9 @@ app.use(express.urlencoded({ extended: true }));
 // Express session
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
@@ -34,15 +33,15 @@ app.use(passport.session());
 app.use(flash());
 
 // Global variables
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
 // Routes
-app.use('/', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
+app.use("/", require("./routes/index.js"));
+app.use("/users", require("./routes/users.js"));
 
 module.exports = app;
