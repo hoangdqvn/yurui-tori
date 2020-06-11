0d4ddfd896752c7c01f3b7e864620ee30e177108
hoangdq <hoangdq.vnist@gmail..com>
2020-06-11 07:08:41 +0700

    Sign up/in/out

---
+++ server.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+// DB Config
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+//Local DB
+// const DB = process.env.DATABASE_LOCAL;
+
+// Connect to MongoDB
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
