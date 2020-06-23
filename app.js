b15cdc202c928c4002b609ccd904ceda703b02b2
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2020-06-23 19:36:11 +0700

    Port Birds

--- app.js
+++ app.js
@@ -44,4 +44,46 @@ app.use(function (req, res, next) {
 app.use("/", require("./routes/index.js"));
 app.use("/users", require("./routes/users.js"));
 
+// Birds
+var
+	http 	= require('http'),
+	path 	= require('path'),
+	Const   = require('./sharedConstants').constant,
+	
+  game 	= require('./game_files/game');
+  
+var
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  methodOverride = require('method-override'),
+  bodyParser = require('body-parser'),
+  errorHandler = require('errorhandler');
+
+// all environments
+app.set('port', Const.SERVER_PORT);
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(errorHandler());
+}
+
+// Route to get shared const file
+app.get('/sharedConstants.js', function(req, res) {
+  res.sendFile(__dirname + '/sharedConstants.js');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});	
+
+game.startServer();
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+var Const = require('../sharedConstants').constant;
 
 // Welcome Page
-router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome', {
+  wsAddress: Const.SOCKET_ADDR + ':' + Const.SOCKET_PORT
+}));
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
